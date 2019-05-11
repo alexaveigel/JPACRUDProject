577ba1c393cf21a369b494e4c9c40bfdfbbe2c87
aveigel <alexaveigel@yahoo.com>
2019-05-11 12:28:48 -0600

    ..

--- UniverseMVCBoot/src/main/java/com/skilldistillery/universe/controllers/StarController.java
+++ UniverseMVCBoot/src/main/java/com/skilldistillery/universe/controllers/StarController.java
@@ -1,6 +1,10 @@
 package com.skilldistillery.universe.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,14 +14,24 @@ import com.skilldistillery.universe.data.UniDAO;
 import com.skilldistillery.universe.entities.Star;
 @Controller
 public class StarController {
+	@Autowired
 	private UniDAO dao;
+	
+	@RequestMapping(path="/")
+	public String index(Model model) {
+		List<Star> stars = dao.findAll();
+		model.addAttribute("star", stars);
+		System.out.println(stars);
+	  return "WEB-INF/index.jsp";
+	  // return "index"; // if using a ViewResolver.
+	}
+	
 	
 	@RequestMapping(path = "getStar.do", method = RequestMethod.GET)
 	  public ModelAndView getStar(@RequestParam("sid") int sid) {
 	    ModelAndView mv = new ModelAndView();
 
 	    Star star = dao.findById(sid);     
-	    // film is unmanaged after it is outside of the transaction that exists in the DAO
 
 	    mv.addObject("star", star);
 	    mv.setViewName("WEB-INF/uni/show.jsp");
